a5f101a2c990dde2fb88e6b23bc277fdcd310cc2
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2022-12-16 23:19:37 +0530

    added different files for different state slices.
    added different files for different state slices.

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,37 +1,41 @@
 import classes from "./Counter.module.css";
 import { useSelector, useDispatch } from "react-redux";
+import { counterActions } from "../store/counter";
 
 const Counter = () => {
   const dispatch = useDispatch();
-  const counter = useSelector((state) => state.counter);
+  const counter = useSelector((state) => state.counter.counter);
+  const show = useSelector((state) => state.counter.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch(counterActions.increment());
   };
 
   const decrementHandler = () => {
-    dispatch({ type: "decrement" });
+    dispatch(counterActions.decrement());
   };
 
-  const incrementBy5Handler = () => {
-    dispatch({ type: "incrementBy5" });
+  const incrementByAmount = () => {
+    dispatch(counterActions.incrementByAmount(10));
   };
 
-  const decrementBy5Handler = () => {
-    dispatch({ type: "decrementBy5" });
+  const decrementByAmount = () => {
+    dispatch(counterActions.decrementByAmount(10));
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={decrementHandler}>Decrement</button>
-        <button onClick={incrementBy5Handler}>IncrementBy5</button>
-        <button onClick={decrementBy5Handler}>DecrementBy5</button>
+        <button onClick={incrementByAmount}>IncrementBy10</button>
+        <button onClick={decrementByAmount}>DecrementBy10</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
